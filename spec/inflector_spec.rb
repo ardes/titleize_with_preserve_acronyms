cc82de7ad34848cf1c8e45b35ae1e11d8666bb23
Ian White <ian.w.white@gmail.com>
2008-12-05 11:17:41 +1100

    Simplify spec

--- spec/inflector_spec.rb
+++ spec/inflector_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe "Inflector#titleize" do
   it '"hello there" => "Hello There"' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] ||= "test"
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require 'rubygems'
 require 'spec'
